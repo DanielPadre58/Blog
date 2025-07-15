52c2b697c53805251ac5f41198e8b739af57f502
DanielPadre58 <2kupwhite@gmail.com>
2025-07-15 21:03:52 +0100

    Post entity implementation

---
+++ Blog/Domain/Entities/Post.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace Blog.Domain.Entities;
+
+public class Post
+{
+    public int Id { get; set; }
+    [Required] public required string Title { get; set; }
+    public string? Content { get; set; }
+    public string ImageUrl { get; set; }
+    public int Likes { get; set; } = 0;
+    public int Dislikes { get; set; } = 0;
+    public DateTime CreatedAt { get; set; } = DateTime.Now;
+    public DateTime? UpdatedAt { get; set; }
+    public int AuthorId { get; set; }
+    public User Author { get; set; }
+    public ICollection<Tag>? Tags { get; set; }
+    [JsonIgnore] public ICollection<Comment> Comments { get; set; }
+}
