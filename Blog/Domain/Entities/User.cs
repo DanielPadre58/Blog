13ec4714165c3c73e5bac0d31803a48bde9f60f8
DanielPadre58 <2kupwhite@gmail.com>
2025-07-15 21:04:07 +0100

    User entity implementation

---
+++ Blog/Domain/Entities/User.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace Blog.Domain.Entities;
+
+public class User
+{
+    public int Id { get; set; }
+    [Required] public required string Username { get; set; }
+    [Required] public required string Email { get; set; }
+    [Required] public required string Password { get; set; }
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public DateTime? Birthday { get; set; }
+    [JsonIgnore] public ICollection<Post> Posts { get; set; }
+    [JsonIgnore] public ICollection<Comment> Comments { get; set; }
+    [JsonIgnore] public ICollection<Post> LikedPosts { get; set; }
+}
