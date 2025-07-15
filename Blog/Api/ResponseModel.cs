761c05f2c4aa91f7a8636feb34f4179444ba5979
DanielPadre58 <2kupwhite@gmail.com>
2025-07-15 21:33:11 +0100

    Added a response model for API responses

---
+++ Blog/Api/ResponseModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Net;
+
+namespace Blog.Api;
+
+public class ResponseModel<T>
+{
+    public HttpStatusCode Status { get; set; } = HttpStatusCode.OK;
+    public string? Message { get; set; }
+    public ICollection<T> Data { get; set; } = new List<T>();
+}
--- Blog/Infrastructure/IUserService.cs
+++ Blog/Infrastructure/IUserService.cs
@@ -1,6 +1,8 @@
-﻿namespace Blog.Infrastructure;
+﻿using Blog.Domain.Entities;
+
+namespace Blog.Infrastructure;
 
 public interface IUserService
 {
-    
+    public Task<User>? Create(User user);
 }
