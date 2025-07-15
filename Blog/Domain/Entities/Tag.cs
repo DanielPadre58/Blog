cde37009603423f025f0719aa0003112e078e5f8
DanielPadre58 <2kupwhite@gmail.com>
2025-07-15 21:04:34 +0100

    Tag entity implementation

---
+++ Blog/Domain/Entities/Tag.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Blog.Domain.Entities;
+
+public class Tag
+{
+    public int Id { get; set; }
+    [Required] public required string Name { get; set; }
+    public int Usages { get; set; } = 1;
+}
