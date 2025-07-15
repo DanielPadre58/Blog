6214c2a8d57ec14c249373863854c2c76f4281cd
DanielPadre58 <2kupwhite@gmail.com>
2025-07-15 21:03:35 +0100

    Added "Tags" table

---
+++ Blog/Migrations/20250715195822_TagsTableImplementation.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Blog.Migrations
+{
+    /// <inheritdoc />
+    public partial class TagsTableImplementation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Images",
+                table: "Posts");
+
+            migrationBuilder.DropColumn(
+                name: "Tags",
+                table: "Posts");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ImageUrl",
+                table: "Posts",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateTable(
+                name: "Tags",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Usages = table.Column<int>(type: "int", nullable: false),
+                    PostId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tags", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Tags_Posts_PostId",
+                        column: x => x.PostId,
+                        principalTable: "Posts",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tags_PostId",
+                table: "Tags",
+                column: "PostId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Tags");
+
+            migrationBuilder.DropColumn(
+                name: "ImageUrl",
+                table: "Posts");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Images",
+                table: "Posts",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Tags",
+                table: "Posts",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
