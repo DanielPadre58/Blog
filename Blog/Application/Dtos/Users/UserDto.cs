2048ae3b9146ac374f767af62cd85b89c0cffb17
DanielPadre58 <2kupwhite@gmail.com>
2025-07-16 23:00:07 +0100

    User DTO implementation

---
+++ Blog/Application/Dtos/Users/UserDto.cs
@@ -0,0 +1,17 @@
+﻿namespace Blog.Application.Dtos.Users;
+
+public record UserDto(int Id, string Username, string? FirstName, string? LastName, DateTime? Birthday)
+{
+    public UserDto(Domain.Entities.User user) : this(
+        user.Id,
+        user.Username,
+        user.FirstName,
+        user.LastName,
+        user.Birthday
+    ){}
+    
+    public static List<UserDto> ToDtoList(List<Domain.Entities.User> users)
+    {
+        return users.Select(user => new UserDto(user)).ToList();
+    }
+}
