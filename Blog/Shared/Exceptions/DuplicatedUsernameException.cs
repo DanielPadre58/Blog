8f260217aed84c38d62b726b6d483066500b6589
DanielPadre58 <2kupwhite@gmail.com>
2025-07-18 00:20:24 +0100

    Exception when trying to create a user with an already existent username

---
+++ Blog/Shared/Exceptions/DuplicatedUsernameException.cs
@@ -0,0 +1,8 @@
+﻿namespace Blog.Shared.Exceptions;
+
+public class DuplicatedUsernameException : Exception
+{
+    public DuplicatedUsernameException() : base("A user with this username already exists") { }
+    
+    public DuplicatedUsernameException(string username) : base($"The username '{username}' is already taken. Please choose a different username.") { }
+}
