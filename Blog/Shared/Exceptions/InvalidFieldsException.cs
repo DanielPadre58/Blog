9ee2573f03011d9f55f76e1f301d40b6c5f225db
DanielPadre58 <2kupwhite@gmail.com>
2025-07-18 00:19:44 +0100

    Exception for fields with invalid values

---
+++ Blog/Shared/Exceptions/InvalidFieldsException.cs
@@ -0,0 +1,11 @@
+﻿namespace Blog.Shared.Exceptions;
+
+public class InvalidFieldsException : Exception
+{
+    public InvalidFieldsException() : base("Some fiels have been given invalid values"){ }
+
+    public InvalidFieldsException(string message) : base(message) { }
+    
+    public InvalidFieldsException(string message, string propertyName) 
+        : base($"Invalid value for property '{propertyName}': {message}") { }
+}
