c99e19165214de9db96862274e31f70373545ce1
DanielPadre58 <2kupwhite@gmail.com>
2025-07-18 20:24:58 +0100

    Post DTO implementation

---
+++ Blog/Application/Dtos/Posts/PostDto.cs
@@ -0,0 +1,17 @@
+﻿using Blog.Application.Dtos.Users;
+using Blog.Domain.Entities;
+
+namespace Blog.Application.Dtos.Posts;
+
+public record PostDto(
+    int Id,
+    string Title,
+    string Content,
+    string ImageUrl,
+    int Likes,
+    int Dislikes,
+    DateTime CreatedAt,
+    DateTime UpdatedAt,
+    UserDto author,
+    Tag[] tags
+);
