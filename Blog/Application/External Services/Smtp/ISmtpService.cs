363bd0484e8d61586b2da784f6562276bf7db438
DanielPadre58 <2kupwhite@gmail.com>
2025-07-21 00:01:06 +0100

    Smtp service implementation for sending emails to the user

---
+++ Blog/Application/External Services/Smtp/ISmtpService.cs
@@ -0,0 +1,6 @@
+﻿namespace Blog.Application.External_Services;
+
+public interface ISmtpService
+{
+    public Task SendVerificationCodeAsync(string email, string code);
+}
---
+++ Blog/Application/External Services/Smtp/SmtpService.cs
@@ -0,0 +1,40 @@
+﻿using System.Net.Mail;
+using MailKit;
+using MailKit.Net.Smtp;
+using MailKit.Security;
+using MimeKit;
+using SmtpClient = MailKit.Net.Smtp.SmtpClient;
+
+namespace Blog.Application.External_Services;
+
+public class SmtpService(IConfiguration configuration) : ISmtpService
+{
+    public async Task SendVerificationCodeAsync(string email, string code)
+    {
+        var message = new MimeMessage ();
+        message.From.Add (new MailboxAddress ("BlogApi", configuration["Smtp:From"]));
+        message.To.Add (new MailboxAddress ("User", email));
+        message.Subject = "Verification Code";
+
+        message.Body = new TextPart ("plain") {
+            Text = @"Your verification code is: " + code +
+                   "\n\nNote: This code is valid for 10 minutes."
+        };
+        
+        await SendMessage(message);
+    }
+
+    private async Task SendMessage(MimeMessage message)
+    {
+        using var client = new SmtpClient();
+        
+        await client.ConnectAsync(
+            configuration["Smtp:Host"],
+            int.Parse(configuration["Smtp:Port"]),
+            SecureSocketOptions.None);
+        
+        await client.SendAsync(message);
+        
+        await client.DisconnectAsync(true);
+    }
+}
