ec05ad63b3606bacd5a8b377f610927bc2d8a46f
DanielPadre58 <2kupwhite@gmail.com>
2025-07-21 21:59:56 +0100

    User validation migration update

---
+++ Blog/Migrations/20250721183837_UserValidation.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Blog.Migrations
+{
+    /// <inheritdoc />
+    public partial class UserValidation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "verified",
+                table: "Users",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "verified",
+                table: "Users");
+        }
+    }
+}
