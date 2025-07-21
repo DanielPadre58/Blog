15f339daa1482eebec9fb43dd31f443127bec0f7
DanielPadre58 <2kupwhite@gmail.com>
2025-07-21 20:27:44 +0100

    Exception for trying to acess a unverified user data

---
+++ Blog/Shared/Exceptions/UnverifiedUserException.cs
@@ -0,0 +1,12 @@
+﻿namespace Blog.Shared.Exceptions;
+
+public class UnverifiedUserException : Exception
+{
+    public UnverifiedUserException() : base("User is not verified, therefore cannot perform this action.")
+    {
+    }
+    
+    public UnverifiedUserException(string message) : base(message)
+    {
+    }
+}
