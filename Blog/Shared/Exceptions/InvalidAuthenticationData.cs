72ebb836782d7b32d207d4ebb8a57724d068d57a
DanielPadre58 <2kupwhite@gmail.com>
2025-07-23 02:39:26 +0100

    Exception for wrong user authentication data

---
+++ Blog/Shared/Exceptions/InvalidAuthenticationData.cs
@@ -0,0 +1,12 @@
+﻿namespace Blog.Shared.Exceptions;
+
+public class InvalidAuthenticationData : Exception
+{
+    public InvalidAuthenticationData() : base("Username or password is invalid.")
+    {
+    }
+    
+    public InvalidAuthenticationData(string message) : base(message)
+    {
+    }
+}
