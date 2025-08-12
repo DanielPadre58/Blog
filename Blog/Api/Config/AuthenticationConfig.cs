06d76b063417ef175c0325ece6adeae5a795fd98
DanielPadre58 <2kupwhite@gmail.com>
2025-08-12 20:44:39 +0100

    Separated Authentication configuration to a new class

---
+++ Blog/Api/Config/AuthenticationConfig.cs
@@ -0,0 +1,57 @@
+﻿using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Blog.Api.Config;
+
+public class AuthenticationConfig
+{
+    private readonly IServiceCollection _services;
+    private readonly IConfiguration _configuration;
+    
+    public AuthenticationConfig(IServiceCollection services, IConfiguration configuration)
+    {
+        _services = services;
+        _configuration = configuration;
+        
+        AddAuthentication();
+    }
+
+    private void AddAuthentication()
+    {
+        _services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidIssuer = _configuration["AppSettings:Issuer"],
+                    ValidateAudience = true,
+                    ValidAudience = _configuration["AppSettings:Audience"],
+                    ValidateLifetime = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(
+                        Encoding.UTF8.GetBytes(_configuration["AppSettings:Token"]!)),
+                    ValidateIssuerSigningKey = true
+                };
+        
+                options.Events = new JwtBearerEvents
+                {
+                    OnAuthenticationFailed = context =>
+                    {
+                        Console.WriteLine($"Authentication failed: {context.Exception.Message}");
+                        return Task.CompletedTask;
+                    },
+                    OnTokenValidated = (context) =>
+                    {
+                        Console.WriteLine("Token validated successfully.");
+                        return Task.CompletedTask;
+                    },
+                    OnChallenge = context =>
+                    {
+                        Console.WriteLine($"OnChallenge: {context.Error}, {context.ErrorDescription}");
+                        return Task.CompletedTask;
+                    }
+                };
+            });
+    }
+}
--- Blog/Api/Config/Startup.cs
+++ Blog/Api/Config/Startup.cs
@@ -1,78 +1,11 @@
-using System.Text;
 using Blog.Api.Config;
-using Blog.Application.External_Services;
-using Blog.Application.External_Services.Smtp;
-using Blog.Application.Services.Authentication;
-using Blog.Application.Services.Comments;
-using Blog.Application.Services.Posts;
-using Blog.Application.Services.Users;
 using Blog.Domain.DbContext;
-using Blog.Domain.Entities;
-using Blog.Domain.Enums;
-using Blog.Domain.Repositories.Comments;
-using Blog.Domain.Repositories.Posts;
-using Blog.Domain.Repositories.Redis;
-using Blog.Domain.Repositories.Tags;
-using Blog.Domain.Repositories.Users;
 using Blog.Shared.Background;
-using Blog.Shared.Security;
-using Blog.Shared.Validation;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
-using Microsoft.OpenApi.Any;
-using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(c =>
-{
-    c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
-
-    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-    {
-        In = ParameterLocation.Header,
-        Description = "Please enter JWT with Bearer into field",
-        Name = "Authorization",
-        Type = SecuritySchemeType.Http,
-        Scheme = "bearer",
-        BearerFormat = "JWT"
-    });
-
-    c.AddSecurityRequirement(new OpenApiSecurityRequirement
-    {
-        {
-            new OpenApiSecurityScheme
-            {
-                Reference = new OpenApiReference
-                {
-                    Type = ReferenceType.SecurityScheme,
-                    Id = "Bearer"
-                }
-            },
-            new string[] {}
-        }
-    });
-    
-    c.MapType<PostFilter>(() => new OpenApiSchema
-    {
-        Type = "string",
-        Enum = Enum.GetNames(typeof(PostFilter))
-            .Select(n => new OpenApiString(n))
-            .Cast<IOpenApiAny>()
-            .ToList()
-    });
-    
-    c.MapType<UserPostsFilter>(() => new OpenApiSchema
-    {
-        Type = "string",
-        Enum = Enum.GetNames(typeof(UserPostsFilter))
-            .Select(n => new OpenApiString(n))
-            .Cast<IOpenApiAny>()
-            .ToList()
-    });
-});
 
 builder.Services.AddDbContext<BlogContext>(options =>
 {
@@ -83,41 +16,8 @@ builder.Services.AddDbContext<BlogContext>(options =>
 
 builder.Services.AddControllers();
 
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddJwtBearer(options =>
-    {
-        options.TokenValidationParameters = new TokenValidationParameters
-        {
-            ValidateIssuer = true,
-            ValidIssuer = builder.Configuration["AppSettings:Issuer"],
-            ValidateAudience = true,
-            ValidAudience = builder.Configuration["AppSettings:Audience"],
-            ValidateLifetime = true,
-            IssuerSigningKey = new SymmetricSecurityKey(
-                Encoding.UTF8.GetBytes(builder.Configuration["AppSettings:Token"]!)),
-            ValidateIssuerSigningKey = true
-        };
-        
-        options.Events = new JwtBearerEvents
-        {
-            OnAuthenticationFailed = context =>
-            {
-                Console.WriteLine($"Authentication failed: {context.Exception.Message}");
-                return Task.CompletedTask;
-            },
-            OnTokenValidated = (context) =>
-            {
-                Console.WriteLine("Token validated successfully.");
-                return Task.CompletedTask;
-            },
-            OnChallenge = context =>
-            {
-                Console.WriteLine($"OnChallenge: {context.Error}, {context.ErrorDescription}");
-                return Task.CompletedTask;
-            }
-        };
-    });
-
+var authenticationConfig = new AuthenticationConfig(builder.Services, builder.Configuration);
+var swaggerConfig = new SwaggerConfig(builder.Services);
 var dependencyInjector = new DependencyInjector(builder.Services, builder.Configuration);
 
 builder.Services.AddHostedService<ExpiredUsersCleaner>();
